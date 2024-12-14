7c0c81bdf004a875a86fbe7969452a5cb478337d
pemudakoding <pemudakoding@gmail.com>
2024-12-14 23:16:00 +0800

    learn thread spawn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use std::thread;
+    use std::time::Duration;
+
+    #[test]
+    fn test_create_thread() {
+        thread::spawn(|| {
+           for i in 0..=5 {
+               println!("hi number {} from the spawned thread!", i);
+               thread::sleep(Duration::from_secs(1));
+           }
+        });
+
+        println!("Application finished");
+        thread::sleep(Duration::from_secs(7));
+    }
+
+}
